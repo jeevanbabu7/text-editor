d84aa8ce3c3054e7b96caaa531989d1ee2932e6b
jeevanbabu7 <jeevanbabu2710@gmail.com>
2024-10-13 22:02:49 +0530

    api

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'text-editor-indol-seven.vercel.app',
         secure: false,
       },
     },
