04e83a6d248c6f5aaea7c43e72616efadde4cd9d
jeevanbabu7 <jeevanbabu190@gmail.com>
2024-11-14 18:35:07 +0530

    some change

--- server/index.js
+++ server/index.js
@@ -14,17 +14,19 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add any additional headers you need
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin'); // Allows same-origin windows to access each other
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp'); // Restricts embedded content to same-origin or CORS
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 app.options('*', cors()); // Enable CORS for all preflight (OPTIONS) requests
 
 // app.use(cookieParser()) 
